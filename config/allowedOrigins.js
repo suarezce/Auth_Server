d718ae4170591907239d2d85a584f7c2d1781b72
suarezce <suarezce.personal@gamil.com>
2025-06-25 09:17:01 -0400

    Adcuacion de cors allowed 25-06-2025

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 export const allowedOrigins = [
     'https://www.yoursite.com',
-    'http://192.168.1.105:20000',
-    'http://192.168.1.105:20001',
-    'http://192.168.1.105:20003',
+    'http://192.168.1.135:20000',
+    'http://192.168.1.135:20001',
+    'http://192.168.1.135:20003',
     'http://localhost:20000',
     'http://localhost:20001',
     'http://localhost:20003',
